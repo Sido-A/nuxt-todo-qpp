9fb66fd05e0d485ca7c2fa10d9f67d7bde3eaf11
Sido-A <sidonie.abe.uk@gmail.com>
2023-10-05 10:26:11 +0900

    added persist line62

--- stores/todo.ts
+++ stores/todo.ts
@@ -1,59 +1,63 @@
 import { v4 as uuidv4 } from "uuid";
 import { ITodo } from "@/interfaces";
 
-export const useTodoStore = defineStore("todo", () => {
-  const todoList = ref<Array<ITodo>>([]);
+export const useTodoStore = defineStore(
+  "todo",
+  () => {
+    const todoList = ref<Array<ITodo>>([]);
 
-  function countInComplete(): number {
-    let sum = 0;
-    todoList.value.forEach((todo) => {
-      if (!todo.completed) {
-        sum += 1;
-      }
-    });
-    return sum;
-  }
+    function countInComplete(): number {
+      let sum = 0;
+      todoList.value.forEach((todo) => {
+        if (!todo.completed) {
+          sum += 1;
+        }
+      });
+      return sum;
+    }
 
-  function countCompleted(): number {
-    let sum = 0;
-    todoList.value.forEach((todo) => {
-      if (todo.completed) {
-        sum += 1;
-      }
-    });
-    return sum;
-  }
+    function countCompleted(): number {
+      let sum = 0;
+      todoList.value.forEach((todo) => {
+        if (todo.completed) {
+          sum += 1;
+        }
+      });
+      return sum;
+    }
 
-  function addNewTodo(newTodo: string): Array<ITodo> {
-    const newTodoObj: ITodo = {
-      id: uuidv4(),
-      text: newTodo,
-      completed: false,
+    function addNewTodo(newTodo: string): Array<ITodo> {
+      const newTodoObj: ITodo = {
+        id: uuidv4(),
+        text: newTodo,
+        completed: false,
+      };
+      todoList.value.unshift(newTodoObj);
+      return todoList.value;
+    }
+
+    function toggleComplete(id: string): Array<ITodo> {
+      todoList.value.forEach((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+      });
+      return todoList.value;
+    }
+
+    function deleteTodo(id: string): Array<ITodo> {
+      todoList.value = todoList.value.filter((t) => t.id !== id);
+      return todoList.value;
+    }
+
+    return {
+      todoList,
+      addNewTodo,
+      toggleComplete,
+      deleteTodo,
+      countInComplete,
+      countCompleted,
     };
-    todoList.value.unshift(newTodoObj);
-    return todoList.value;
-  }
-
-  function toggleComplete(id: string): Array<ITodo> {
-    todoList.value.forEach((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-    });
-    return todoList.value;
-  }
-
-  function deleteTodo(id: string): Array<ITodo> {
-    todoList.value = todoList.value.filter((t) => t.id !== id);
-    return todoList.value;
-  }
-
-  return {
-    todoList,
-    addNewTodo,
-    toggleComplete,
-    deleteTodo,
-    countInComplete,
-    countCompleted,
-  };
-});
+  },
+  { persist: true }
+);
